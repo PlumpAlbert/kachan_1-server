676dce209a38c2bcd2925634e27358bebb1724da
plump_albert <plumpalbert@gmail.com>
2019-11-18 08:14:53 +0300

    Created routes to test if it's possible to create files on Heroku

--- server.py
+++ server.py
@@ -7,3 +7,16 @@ app = Flask(__name__, static_url_path='', static_folder='build')
 @app.route("/")
 def react_app():
     return app.send_static_file('index.html')
+
+
+@app.route('/create')
+def lol():
+    file = open('test', 'w')
+    file.write('This is a test')
+    return 200
+
+
+@app.route('/get')
+def get():
+    file = open('test', 'r')
+    return file.read()
